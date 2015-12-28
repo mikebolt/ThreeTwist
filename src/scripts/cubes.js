343e7acb36d9cd0ead92a1883be20a2e40b4d395
mikebolt <mikebolt@gmail.com>
2015-12-27 21:55:40 -0800

    Remove logging statement

--- src/scripts/cubes.js
+++ src/scripts/cubes.js
@@ -661,8 +661,6 @@ ThreeTwist.extend( ThreeTwist.Cube.prototype, {
 
     }.bind( this ))
     .start( this.time );
-    
-    console.log('twist started with startTime = ' + this.time + ', duration = ' + duration);
 
   },
 
